3ed880e574bf0e44407c3be4105e04059c51e2d3
Mary <treetrunkapples@gmail.com>
2020-11-27 12:17:21 +0200

    file replacement

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Mary', age: 22 },
-      { name: 'Max', age: 30 },
-      { name: 'Liv', age: 52 }
+      { id: '54365', name: 'Mary', age: 22 },
+      { id: '33254', name: 'Max', age: 30 },
+      { id: '2315464', name: 'Liv', age: 52 }
     ],
     personsShow: false
   }
@@ -16,8 +16,17 @@ class App extends Component {
     this.setState({personsShow: !this.state.personsShow})
   };
 
-  nameChangedHandler = (event) => {
-    console.log(event.target)
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  changePersonNameHandler = (event, index) => {
+    const personsCopy = [...this.state.persons];
+    personsCopy[index].name = event.target.value;
+
+    this.setState({persons: personsCopy});
   }
 
   render() {
@@ -26,10 +35,13 @@ class App extends Component {
     if (this.state.personsShow) {
       persons = (
        <div>
-         { this.state.persons.map(person => {
+         { this.state.persons.map((person, index) => {
            return <Person
                     name={person.name}
-                    age={person.age} />
+                    age={person.age} 
+                    click={() => this.deletePersonHandler(index)}
+                    key={person.id}
+                    change={(e) => this.changePersonNameHandler(e, index)}/>
          }) }
         </div>
       )
